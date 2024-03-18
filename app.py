2f0cc5ffea1da6fe78f38049b9626f7bfc45957c
George0712 <jorge12072002@gmail.com>
2024-03-18 08:05:05 -0500

    enviar mensaje

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from socket import SocketIO
+from flask_socketio import SocketIO
+from flask import render_template
+
+
+app = Flask(__name__)
+socketIO = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketIO.on('mensaje')
+def handle_mensaje(msg):
+    print(msg)
+    socketIO.emit('mensaje', msg)
+    
+if __name__ == '__main__':
+    socketIO.run(app, debug = True)
